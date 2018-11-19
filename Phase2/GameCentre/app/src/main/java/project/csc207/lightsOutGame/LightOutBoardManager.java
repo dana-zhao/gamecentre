e0cdbe8640c62eed20b3c6cbf4ec211206936988
Jinjie Sun <jinjie.sun@mail.utoronto.ca>
2018-11-18 20:19:44 -0500

    Add methods in Light and LightOutBoardManager

--- Phase2/GameCentre/app/src/main/java/project/csc207/lightsOutGame/LightOutBoardManager.java
+++ Phase2/GameCentre/app/src/main/java/project/csc207/lightsOutGame/LightOutBoardManager.java
@@ -38,17 +38,45 @@ class LightOutBoardManager implements Serializable {
      *
      * @return whether all lights in Light Out Board are On
      */
-    boolean allLightsOn(){
+    boolean allLightsOut(){
         for (int i =0; i < lightsOutBoard.numLights()-1; i++){
             int row = i / LightsOutBoard.NUM_ROWS;
             int col = 1 % LightsOutBoard.NUM_COLS;
             Light lightToCheck = lightsOutBoard.getLight(row, col);
-            if (!lightToCheck.getState()){
+            if (lightToCheck.getState()){
                 return false;
             }
         }
         return true;
     }
 
+    /**
+     * return a list of Lights, contains the light and lights around the given light
+     * @param position the position of light to check
+     * @return a list of Lights around and include the give light
+     */
+    public List<Light> getLightsAround(int position){
+        int row = position / LightsOutBoard.NUM_ROWS;
+        int col = position % LightsOutBoard.NUM_COLS;
+        Light lightToCheck = lightsOutBoard.getLight(row,col);
+        Light above = row == 0 ? null : lightsOutBoard.getLight(row - 1, col);
+        Light below = row == LightsOutBoard.NUM_ROWS - 1 ? null : lightsOutBoard.getLight(row + 1, col);
+        Light left = col == 0 ? null : lightsOutBoard.getLight(row, col - 1);
+        Light right = col == LightsOutBoard.NUM_COLS - 1 ? null : lightsOutBoard.getLight(row, col + 1);
+        List<Light> lights = new ArrayList<>();
+        lights.add(lightToCheck);
+        if (above!=null){lights.add(above);}
+        if (below!=null){lights.add(below);}
+        if (left!=null){lights.add(left);}
+        if (right!=null){lights.add(right);}
+        return lights;
+    }
+
+    public void touchToSwitch(int position){
+        List<Light> lightsToSwitch = getLightsAround(position);
+        for(Light light:lightsToSwitch){
+            light.switchLight(light);
+        }
+    }
 
 }
