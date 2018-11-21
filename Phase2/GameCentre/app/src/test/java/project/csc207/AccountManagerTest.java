f26c05bacbd601ef0b13bc2892d1df209172fbd4
danazhao <dana.zhao@mail.utoronto.ca>
2018-11-21 17:54:15 -0500

    added test classes

---
+++ Phase2/GameCentre/app/src/test/java/project/csc207/AccountManagerTest.java
@@ -0,0 +1,50 @@
+package project.csc207;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AccountManagerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getCurrentAccount() {
+    }
+
+    @Test
+    public void hasAllAccount() {
+    }
+
+    @Test
+    public void setCurrentAccount() {
+    }
+
+    @Test
+    public void notNewUser() {
+    }
+
+    @Test
+    public void updateAccount() {
+    }
+
+    @Test
+    public void rightPassword() {
+    }
+
+    @Test
+    public void signUp() {
+    }
+
+    @Test
+    public void setAllAccount() {
+    }
+}
---
+++ Phase2/GameCentre/app/src/test/java/project/csc207/AccountTest.java
@@ -0,0 +1,46 @@
+package project.csc207;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AccountTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getUserName() {
+    }
+
+    @Test
+    public void getScore() {
+    }
+
+    @Test
+    public void getPassword() {
+    }
+
+    @Test
+    public void getBoardManager() {
+    }
+
+    @Test
+    public void setBoardManager() {
+    }
+
+    @Test
+    public void getLightsOutBoardManager() {
+    }
+
+    @Test
+    public void setLightsOutBoardManager() {
+    }
+}
