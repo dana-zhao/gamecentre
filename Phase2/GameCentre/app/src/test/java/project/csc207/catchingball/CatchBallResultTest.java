2b9dc081564790bd4a8753ba3b979ffa5128cb3c
danazhao <dana.zhao@mail.utoronto.ca>
2018-11-28 17:24:57 -0500

    added test classes

---
+++ Phase2/GameCentre/app/src/test/java/project/csc207/catchingball/CatchBallResultTest.java
@@ -0,0 +1,28 @@
+package project.csc207.catchingball;
+
+import android.support.v7.app.AppCompatActivity;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CatchBallResultTest extends AppCompatActivity {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void tryAgain() {
+    }
+}
