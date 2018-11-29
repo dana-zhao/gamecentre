f3641f3a25e4b5a2672cd80804afc5c85ae3a2a3
Tristydai <atonementtristy@gmail.com>
2018-11-29 18:50:47 -0500

    add some unit tests for the accountmanager

--- Phase2/GameCentre/app/src/test/java/project/csc207/AccountTest.java
+++ Phase2/GameCentre/app/src/test/java/project/csc207/AccountTest.java
@@ -10,6 +10,7 @@ public class AccountTest {
 
     @Before
     public void setUp() throws Exception {
+
     }
 
     @After
---
+++ Phase2/GameCentre/app/src/test/java/project/csc207/catchtheballgame/ScoreBoardForCatchingBallTest.java
@@ -0,0 +1,12 @@
+package project.csc207.catchtheballgame;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ScoreBoardForCatchingBallTest {
+
+    @Test
+    public void onCreate() {
+    }
+}
