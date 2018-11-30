86485a02ff184ff38ad050291a71be7bfa948886
Tristydai <atonementtristy@gmail.com>
2018-11-30 09:44:19 -0500

    fix the error of the tile test

--- Phase2/GameCentre/app/src/test/java/project/csc207/slidingtiles/TileTest.java
+++ Phase2/GameCentre/app/src/test/java/project/csc207/slidingtiles/TileTest.java
@@ -7,24 +7,26 @@ import project.csc207.R;
 
 public class TileTest {
 
-    private Tile t1;
-    private Tile t2;
-
-
-    @Test
-    public void constructorTest() {
-        t1 = new Tile(1,1);
-        t2 = new Tile(1);
-
-        assertEquals(1, t1.getId());
-        assertEquals(1, t1.getBackground());
-        assertEquals(2, t2.getId());
-        assertEquals(R.drawable.tile_2, t2.getBackground());
-
-    }
 
     @Test
     public void compareTo() {
+        Tile t1 = new Tile(1,1);
+        Tile t2 = new Tile(2,1);
         assertEquals(1, t1.compareTo(t2));
+    }
+
+    @Test
+    public void getBackground() {
+        Tile t1 = new Tile(2,1);
+        Tile t2 = new Tile(1);
+
+        assertEquals(1, t1.getBackground());
+        assertEquals(R.drawable.tile_2, t2.getBackground());
+    }
+
+    @Test
+    public void getId() {
+        Tile t1 = new Tile(2,1);
+        assertEquals(2, t1.getId());
     }
 }
