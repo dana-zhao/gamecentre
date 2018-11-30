635ba7c1f10b7dc92b7f2d8d301f6dddbb025f47
Yining Zhang <elaineyining.zhang@mail.utoronto.ca>
2018-11-30 03:04:18 -0500

    board manager unit test

--- Phase2/GameCentre/app/src/test/java/project/csc207/slidingtiles/BoardManagerTest.java
+++ Phase2/GameCentre/app/src/test/java/project/csc207/slidingtiles/BoardManagerTest.java
@@ -7,83 +7,108 @@ import static junit.framework.TestCase.*;
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class BoardManagerTest {
-    private final int EVEN_SIZE = 4;
-    private final int ODD_SIZE = 5;
-    private SlidingTilesHelpMethods help = new SlidingTilesHelpMethods();
+    /**
+     * Size of board
+     */
+    private final int SIZE = 4;
 
-    /*@Before
+    /**
+     * Test board manager
+     */
+    private BoardManager testBoardManager = new BoardManager(SIZE);
+
+    @Before
     public void setUp() {
+        //Create solved board
         List<Tile> tiles = new ArrayList<>();
-        Integer[] testTileNumbers = {12, 1, 10, 2, 7, 11, 4, 14, 5, 16, 9, 15, 8, 13, 6, 3};
-        for (int tileNum : testTileNumbers) {
-            tiles.add(new Tile(tileNum));
+        for (int i = 0; i < SIZE * SIZE - 1; i++) {
+            tiles.add(new Tile(i));
         }
-        Board testEvenBoard = new Board(tiles);
-        testEvenBoardManager.setBoard(testEvenBoard);
-    }*/
-
-    //Need to rewrite, not sure how to test meaningfully + seems to be running infinitely
-    @Test
-    public void testBoardManagerConstructor() {
-        //Test that odd sized board is solvable
-        BoardManager testBoardManager = new BoardManager(ODD_SIZE);
-        Board testBoard = testBoardManager.getBoard();
-        List<Integer> tileNums = new ArrayList<>();
-        for (Tile t: testBoard){
-            tileNums.add(t.getId());
-        }
-        int inversions = SlidingTilesHelpMethods.getInversions(tileNums, ODD_SIZE);
-        assertEquals(inversions % 2, 0);
-        //Test that even sized board is solvable
-        testBoardManager = new BoardManager(EVEN_SIZE);
-        testBoard = testBoardManager.getBoard();
-        tileNums = new ArrayList<>();
-        for (Tile t: testBoard){
-            tileNums.add(t.getId());
-        }
-        inversions = SlidingTilesHelpMethods.getInversions(tileNums, EVEN_SIZE);
-        if (testBoard.getBlankRow() % 2 == 1) {
-            assertEquals(inversions % 2, 1);
-        }
-        else {
-            assertEquals(inversions % 2, 0);
-        }
+        tiles.add(new Tile(24));
+        Board testSolvedBoard = new Board(tiles);
+        testBoardManager.setBoard(testSolvedBoard);
     }
 
+    /**
+     * Test puzzleSolved method correctly checks for solved and unsolved boards.
+     */
     @Test
     public void testPuzzleSolved() {
         //Test solved board
-        List<Tile> tiles = new ArrayList<>();
-        for (int i = 0; i < EVEN_SIZE * EVEN_SIZE; i++) {
-            tiles.add(new Tile(i));
-        }
-        Board testSolvedBoard = new Board(tiles);
-        BoardManager testSolvedBoardManager = new BoardManager(EVEN_SIZE);
-        testSolvedBoardManager.setBoard(testSolvedBoard);
-        assertTrue(testSolvedBoardManager.puzzleSolved());
+        assertTrue(testBoardManager.puzzleSolved());
         //Test unsolved board
-        tiles.clear();
-        Integer[] testTileNumbers = {0, 1, 2, 3, 4, 6, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-        for (int t: testTileNumbers) {
+        List<Tile> tiles = new ArrayList<>();
+        Integer[] testTileNumbers = {0, 1, 2, 3, 4, 6, 5, 7, 8, 9, 10, 11, 12, 13, 14, 24};
+        for (int t : testTileNumbers) {
             tiles.add(new Tile(t));
         }
-        testSolvedBoard = new Board(tiles);
-        testSolvedBoardManager.setBoard(testSolvedBoard);
-        assertFalse(testSolvedBoardManager.puzzleSolved());
+        Board testUnsolvedBoard = new Board(tiles);
+        testBoardManager.setBoard(testUnsolvedBoard);
+        assertFalse(testBoardManager.puzzleSolved());
     }
 
-    /*@Test
-    public void testNumOfInversions() {
-        Integer[] testTileNumbers = {12, 1, 10, 2, 7, 11, 4, 14, 5, 16, 9, 15, 8, 13, 6, 3};
-        List<Integer> tileArray = new ArrayList<>();
-        tileArray.addAll(Arrays.asList(testTileNumbers));
-        int num = testBoardManager.numOfInversions(tileArray, 4);
-        assertEquals(num, 49);
+    /**
+     * Test isValidTap method correctly returns if the tile at a certain position is valid
+     */
+    @Test
+    public void testIsValidTap() {
+        //Test valid tiles
+        assertTrue(testBoardManager.isValidTap(14));
+        assertTrue(testBoardManager.isValidTap(11));
+        //Test random invalid tile
+        assertFalse(testBoardManager.isValidTap(0));
+        //Test position of blank tile itself
+        assertFalse(testBoardManager.isValidTap(15));
     }
-    */
 
+    /**
+     * Test touchMove method correctly swaps tiles if and only if the tap is valid
+     */
+    @Test
+    public void testTouchMove() {
+        //Test that blank tile and to its left are swapped
+        testBoardManager.touchMove(14);
+        int swappedTileOne = testBoardManager.getBoard().getTile(3, 2).getId();
+        int swappedTileTwo = testBoardManager.getBoard().getTile(3, 3).getId();
+        assertEquals(25, swappedTileOne);
+        assertEquals(15, swappedTileTwo);
+        testBoardManager.touchMove(13);
+        swappedTileOne = testBoardManager.getBoard().getTile(3, 1).getId();
+        swappedTileTwo = testBoardManager.getBoard().getTile(3, 2).getId();
+        assertEquals(25, swappedTileOne);
+        assertEquals(14, swappedTileTwo);
+        //Test that the tiles are not swapped when the tap is not valid
+        testBoardManager.touchMove(0);
+        swappedTileOne = testBoardManager.getBoard().getTile(3, 1).getId();
+        swappedTileTwo = testBoardManager.getBoard().getTile(0, 0).getId();
+        assertEquals(25, swappedTileOne);
+        assertEquals(1, swappedTileTwo);
+    }
+
+    /**
+     * Test isGameOver method correctly returns whether or not the game is over
+     */
+    @Test
+    public void testIsGameOver() {
+        testBoardManager.touchMove(14);
+        assertFalse(testBoardManager.isGameOver());
+        testBoardManager.touchMove(15);
+        assertTrue(testBoardManager.isGameOver());
+    }
+
+    /**
+     * Test countScore gives lower score to player if they take more moves
+     */
+    @Test
+    public void testCountScore() {
+        testBoardManager.touchMove(14);
+        int higherScore = testBoardManager.countScore();
+        testBoardManager.touchMove(13);
+        int lowerScore = testBoardManager.countScore();
+        //The higher score should be higher than the lower score by one since it took one less move
+        assertEquals(higherScore - lowerScore, 1);
+    }
 }
